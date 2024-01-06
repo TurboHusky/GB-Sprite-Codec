4c64b6af592c227d813c7381f49a3b8ab81e244c
Ben P <ben.passman@outlook.com>
2024-01-06 21:44:42 +0000

    Added test for freeing sprite data

--- test/sprite_test.c
+++ test/sprite_test.c
@@ -9,17 +9,29 @@
 static void read_test_file(void **state)
 {
     (void)state;
-    struct sprite_t result = load_sprite("../../test/test_images/test_1x1_01.bin");
-    assert_non_null(result.image);
-    assert_int_equal(result.width, 1);
-    assert_int_equal(result.height, 1);
-    free_sprite(&result);
+    struct sprite_t sprite = load_sprite("../../test/test_images/test_1x1_01.bin");
+    assert_non_null(sprite.image);
+    assert_int_equal(sprite.width, 1);
+    assert_int_equal(sprite.height, 1);
+    free_sprite(&sprite);
+
+}
+
+static void free_sprite_resources(void **state)
+{
+    (void)state;
+    struct sprite_t sprite = load_sprite("../../test/test_images/test_1x1_01.bin");
+    free_sprite(&sprite);
+    assert_null(sprite.image);
+    assert_uint_equal(sprite.width, 0);
+    assert_uint_equal(sprite.height, 0);    
 }
 
 int main()
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(read_test_file)
+        cmocka_unit_test(read_test_file),
+        cmocka_unit_test(free_sprite_resources)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
